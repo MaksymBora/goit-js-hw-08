313a6a224d08a1b0c5317841f9ee38d4f20abc19
MaksymBora <maxboraod@gmail.com>
2023-06-01 17:51:34 +0200

    add first hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,41 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}" >
+                <img class="gallery__image" src="${preview}"  alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join('');
+}
+
+gallery.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+
+  const { target } = e;
+
+  if (!target.classList.contains('gallery__image')) {
+    return;
+  }
+}
+
+let modalImg = new SimpleLightbox('.gallery a', {
+  doubleTapZoom: '1.5',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
