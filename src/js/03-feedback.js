6c110a0a41dd3773dd674b2a14cb71a15ad89440
MaksymBora <maxboraod@gmail.com>
2023-06-02 22:30:34 +0200

    add 3rd tast

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+  email: document.querySelector('.feedback-form input[name="email"]'),
+};
+
+const formData = {};
+
+refs.form.addEventListener('input', throttle(addInputDataToLocalStorage, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+onTextAreaInput();
+
+function addInputDataToLocalStorage(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(formData));
+}
+
+function onTextAreaInput() {
+  //   console.log(JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY)));
+
+  const localStorageData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+
+  if (localStorageData.message) {
+    refs.textarea.value = localStorageData.message;
+  }
+
+  if (localStorageData.email) {
+    refs.email.value = localStorageData.email;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.target.reset();
+  const localStorageData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+
+  if (localStorageData) {
+    console.log(localStorageData);
+  }
+
+  localStorage.removeItem(FEEDBACK_FORM_KEY);
+}
